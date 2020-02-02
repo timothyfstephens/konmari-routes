6d5a973cc79b01fff11c443f3c8b47a8855f400f
Timothy Stephens <timothy.f.stephens@gmail.com>
2020-02-01 20:05:03 -0500

    Add yard documentation to all files

--- lib/konmari/routes/configuration.rb
+++ lib/konmari/routes/configuration.rb
@@ -1,14 +1,18 @@
 module Konmari
   module Routes
+    # The configuration object holding the necessary data for {Loader} to do its thing.
     class Configuration
-      attr_accessor :application,
-                    :routes_path,
-                    :logger
+      attr_writer :application
+      # @return [Pathname] the folder containing the routing hierarchy to build
+      attr_accessor :routes_path
+      attr_writer :logger
 
+      # @return [Logger] the logger to use for debugging purposes (optional, defaults to +Rails.logger+ if using Rails)
       def logger
         @logger || (Object.const_defined?(:Rails) ? Rails.logger : nil)
       end
 
+      # @return [Application] the application to build these routes for (defaults to +Rails.application+ if using Rails)
       def application
         @application || (Object.const_defined?(:Rails) ? Rails.application : nil)
       end
--- lib/konmari/routes/loader.rb
+++ lib/konmari/routes/loader.rb
@@ -1,28 +1,66 @@
 module Konmari
   module Routes
+    # Given a routes directory, recursively load all routes.
+    #
+    # Recursively loads all routes in a directory following this algorithm:
+    # 1. Load the files in each directory that match PRIORITY_FILES in order listed
+    # 2. Load each directory, opening a *new namespace* matching the directory name, and start back at step 1
+    # 3. Load all other files in alphabetical order
+    #
+    # Be aware that any routes will be respected in the order they are loaded
+    # Routes files must use extension +.routes+
+    #
+    # Example file structure:
+    #     |- routes/
+    #     |  |- index.routes
+    #     |  |- comments.routes
+    #     |  |- users/
+    #     |     |- index.routes
+    #     |     |- friends.routes
+    #
+    # Analogous routes definitions
+    #
+    #     application.routes.draw do
+    #       # all index routes
+    #       # all comments routes (likely a resource, *must* have first code line of `resource/namespace :comments`)
+    #       namespace :users do
+    #         # all index routes from `index.routes` file in `users/`
+    #         # all friends routes from `friends.routes` in `users/`
+    #       end
+    #     end
+    #
+    # The +friends.routes+ file could then be as simple as:
+    #
+    #    # routes/users/friends.routes
+    #    # NOTE: the resource matches the filename
+    #    resources :friends, only: [:index, :create, :delete], path: :my_friends # or any other options passed to resource(s)
+    #
+    # Point being, declaring the namespace is unnecessary.  This gives us the _huge_ advantage of being able to have our routes
+    # files _exactly_ match the file heirarchy of our controllers.  We also have the flexibility, through the prioritized files,
+    # to add any other routes we might need without being restrained by the filename constraint.
+    #
     class Loader
-      # Given a routes directory, recursively load all routes, following this:
-      # 1. Load the files in each directory that match PRIORITY_FILES in order listed
-      # 2. Load each directory, opening a namespace matching the directory name, and start back at step 1
-      # 3. Load all other files in alphabetical order
-      #
-      # Be aware that any routes will be respected in the order they are loaded
-      # Routes files must name `.routes` or `.rb`
 
-      EXPECTED_FILENAME_REGEX = /:(?<fname>.*?)\W/ unless const_defined?(:EXPECTED_FILENAME_REGEX)
-      unless const_defined?(:PRIORITY_FILES)
+      EXPECTED_FILENAME_REGEX = /:(?<fname>.*?)\W/ unless Object.const_defined?(:EXPECTED_FILENAME_REGEX)
+      unless Object.const_defined?(:PRIORITY_FILES)
+        # The list of filenames which are exempt from the resource name matching rule, and are always loaded first
+        # in any directory they are seen in.
         PRIORITY_FILES = [
           :priority,
           :redirects,
           :index
         ].freeze
       end
+      private_constant :EXPECTED_FILENAME_REGEX
 
       def initialize(config)
         @app           = config.application
         @routes_folder = config.routes_path
         @logger        = config.logger
+        build_routes
       end
+
+      private
 
       def build_routes
         do_router = -> (router) { load_routes(router) }
@@ -31,8 +69,6 @@ module Konmari
         # doesn't allow us to call load routes directly
         @app.routes.draw { do_router.call(self) }
       end
-
-      private
 
       def load_routes(router)
         @router = router
@@ -105,6 +141,16 @@ module Konmari
       end
     end
 
+    # Raised when a filename does not have the correct extension, or the first detected line of code does not match the filename
+    #
+    # Eg:
+    #
+    #    # routes/thing/hello.route
+    #    resource :world
+    #
+    # would raise this error for two reasons:
+    # 1. The extension should be +.routes+
+    # 2. The filename should be +world.routes+ or the resource should be +:hello+
     class FilenameError < StandardError; end
   end
 end
